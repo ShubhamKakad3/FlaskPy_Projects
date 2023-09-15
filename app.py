7fb82adac85d3c2cd821da205a5b9b8729915823
shubham <shubhamkakad10x@gmail.com>
2023-09-15 15:30:44 +0530

    new html1 file added
    thats it

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-from flask import Flask,request,url_for,render_template,redirect
+from flask import Flask,request,url_for,render_template,redirect,jsonify
 from datetime import datetime
 # from flask_sqlalchemy import SQLALchemy
 
 app = Flask(__name__)
 
-
-# @app.route('/')
-# def index():
-#     return 'Hello from shubham!'
+JOBS = [
+   {
+   'id' : 1,
+   'title' : 'data analyst',
+   'location':'banglore,india',
+   'salory':'Rs. 11,00,000'
+   },
+   {
+   'id' : 2,
+   'title' : 'data scientist',
+   'location':'remote',
+    # 'salory':'usd 120,000'
+   },
+   {
+   'id' : 3,
+   'title' : 'data architecture',
+   'location':'america,new york city',
+   'salory':'usd 130,000'
+   },
+   {
+   'id' : 1,
+   'title' : 'frontend developer',
+   'location':'banglore,india',
+   'salory':'Rs. 15,00,000'
+   }
+]
 
 @app.route('/')
 def about():
-    return render_template('index.html')
+    return render_template('index.html' ,jobs = JOBS , ab = 'CAREER' )
+
+@app.route('/api/jobs')
+def jobs():
+   return jsonify(JOBS)
 
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port=81 , debug=True)
